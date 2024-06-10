19b50de6ad5a0ca546f26c45d8cf4fc1c449ef26
ryan <ryanroberts562@gmail.com>
2024-06-10 19:02:51 +0300

    init

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import instaStorySearch from './assets/insta-story-search.png';
+  import Form from './lib/Form.svelte';
+</script>
+
+<main>
+  <div class="logo-wrap">
+    <img src={instaStorySearch} class="logo" alt="Logo" />
+  </div>
+  <h1>Insta Story Searcher</h1>
+  <p>You simp</p>
+  <Form />
+</main>
+
+<style>
+  .logo-wrap {
+    margin-bottom: 1px;
+  }
+  h1 {
+    margin: 10px 0px;
+  }
+  p {
+    opacity: 0.5;
+    font-style: italic;
+  }
+  .logo {
+    height: 6em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+</style>
---
+++ src/lib/Form.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  function search() {
+    console.log('searching');
+  }
+</script>
+
+<div class="form-wrap">
+  <label class="input-wrap">
+    <input type="text" name="query" placeholder="Search a viewer" id="" />
+  </label>
+  <button on:click={search}> Search 🚀 </button>
+</div>
+
+<style>
+  .form-wrap {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .input-wrap {
+    display: flex;
+    flex-direction: column;
+  }
+
+  input {
+    padding: 10px 5px;
+    font-size: medium;
+    font-weight: 500;
+    margin: auto;
+    text-align: center;
+    border-radius: 10px;
+    border: 1px solid grey;
+    margin-bottom: 10px;
+  }
+  button {
+    font-weight: 700;
+    border: 1px solid rgb(213, 213, 213);
+  }
+</style>
